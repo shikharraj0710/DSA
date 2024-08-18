5bdef2576e92b2718da0016d02d1094e81b325d0
shikharraj0710 <shikharraj921@gmail.com>
2024-08-18 18:49:39 +0530

    Stack and notes

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -23,3 +23,31 @@ print(arr)
 # This process is then continued to find the second largest and place it and so on until the data is sorted.
 
 # Time Complexity: O(N2)
+
+# Replace ___ with your code
+
+def swap(arr, i, j):
+    arr[i], arr[j] = arr[j], arr[i]
+
+def bubble_sort(lst):
+    # write your code here
+    n = len(lst)
+    for i in range(0, n - 1):
+        swapped = False
+        for j in range(0, n - i - 1):
+            if lst[j] >= lst[j+1]:
+                swap(lst, j, j + 1)
+                swapped = True
+        if swapped == False:
+            break
+
+    return lst
+
+# take inputs and convert it to a list
+data_list = list(map(int, input().split()))
+
+# call the bubble sort function
+sorted_list = bubble_sort(data_list)
+
+# print the sorted list
+print(sorted_list)
